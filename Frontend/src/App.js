fba66da92af067cdf2ed4b3c79bf45fa8d78cac1
Ali Farmand <alifarmandkn@gmail.com>
2018-01-22 18:26:48 +0100

    Creates post markup and adds icon library

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
 import Navigation from './Components/navbar'
+import ThumbsUp from 'react-icons/lib/fa/thumbs-up'
+import ThumbsDown from 'react-icons/lib/fa/thumbs-down'
+
 //import { Route } from 'react-router-dom'
 class App extends Component {
-
-  state = {
-    pages: {
-      root: '/',
-      category: '/category',
-    },
-  }
-
   render () {
     return (
       <div className='App' >
@@ -30,7 +25,27 @@ class App extends Component {
             <h2 className='grid__title' >Posts</h2 >
             <ul className='postList' >
               <li className='postList__item' >
-                <div ><p >Lorem ipsum and more of those things!</p ></div >
+                <div className='post' >
+                  <div className='post__header'>
+                    <p className='post__header__name'>Ali Farmand</p>
+                    <p className='post__header__time'><small>2 Hours ago</small></p>
+                  </div>
+                  <div className='post__body'>
+                    <h2 className='post__body__headline'>Headline and stuff</h2>
+                    <p className='post__body__content'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  </div>
+                  <div className='post__footer'>
+                    <div className='post__footer__left'>
+                      <p>1 <small>Like</small></p>
+                      <ThumbsUp className='icons' />
+                      <ThumbsDown className='icons' />
+                    </div>
+                    <div className='post__footer__right'>
+                      <p>23 <small>Comments</small></p>
+                      <button className='button'>Comment</button>
+                    </div>
+                  </div>
+                </div >
               </li >
               <li className='postList__item' >2</li >
               <li className='postList__item' >3</li >
